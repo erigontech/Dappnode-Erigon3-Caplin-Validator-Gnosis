91b5da6273f2846466727324f0e7ed6a3bec0a42
Giulio <giulio.rebuffo@gmail.com>
2024-12-05 19:15:47 +0100

    save

--- beacon-chain/entrypoint.sh
+++ beacon-chain/entrypoint.sh
@@ -44,6 +44,6 @@ exec erigon --datadir=${DATADIR} \
     --authrpc.vhosts=* \
     --db.size.limit=8TB \
     --prune.mode=${PRUNE_MODE} \
-    --caplin.max-inbound-traffic-per-peer=4MB \
-    --caplin.max-outbound-traffic-per-peer=4MB \
+    --caplin.max-inbound-traffic-per-peer=1.5MB \
+    --caplin.max-outbound-traffic-per-peer=1.5MB \
     ${EXTRA_OPTs}
--- validator/entrypoint.sh
+++ validator/entrypoint.sh
@@ -53,7 +53,7 @@ exec -c lighthouse \
     --enable-doppelganger-protection \
     --init-slashing-protection \
     --datadir /root/.lighthouse \
-    --beacon-nodes ${BEACON_API_PORT} \
+    --beacon-nodes ${BEACON_NODE_ADDR} \
     --graffiti="$GRAFFITI" \
     --http \
     --http-address 0.0.0.0 \
